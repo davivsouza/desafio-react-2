8d607b002747941f2e64c7f98560b4a8dfcc777a
Davi Vasconcelos Souza <81455819+davivsouza@users.noreply.github.com>
2022-12-26 17:46:03 +0000

    finalizando desafio

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import './App.css'
+function App() {
+  const [clicks, setClicks] = useState([])
+  const [lastDots, setLastDots] = useState([])
+
+  const colors = ['red', 'blue', 'green', 'purple', 'yellow', 'white']
+
+  function handleUndo() {
+    const dots = [...clicks]
+    dots.splice(dots.length - 1, 1)
+    setClicks(dots)
+  }
+
+  function handleRedo() {
+    const dots = [...clicks]
+
+    if(dots.length === lastDots.length){
+      return;
+    }
+    dots.push(lastDots[dots.length])
+    setClicks(dots)
+  }
+
+  function handleMousePosition(ev) {
+    const x = ev.clientX
+    const y = ev.clientY
+
+    const randomColor = colors[Math.floor(Math.random() * 6)]
+
+    setClicks(prevState => [...prevState, {
+      x,
+      y,
+      color: randomColor
+    }])
+    setLastDots(prevState => [...prevState, {x, y, color: randomColor}])
+  }
+  return (
+    <>
+      <button onClick={handleUndo}>desfazer</button>
+      <button onClick={handleRedo}>refazer</button>
+      <div className="container" onClick={handleMousePosition}>
+
+        {clicks.map((click, idx) => (
+          <div
+            key={idx}
+            className='clicks-div'
+            style={{
+              left: click.x,
+              top: click.y,
+              background: click.color
+            }}
+          />
+
+        ))}
+      </div>
+    </>
+
+  )
+}
+
+export default App
